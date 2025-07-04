8ee04fc27e6ba255751714663306d8f36a48970e
nuno <nunopunkas@gmail.com>
2025-07-04 12:14:41 +0200

    update requetes

--- requetes_phase2.sql
+++ requetes_phase2.sql
@@ -23,4 +23,8 @@ LEFT JOIN scores AS s ON j.id = s.id_joueur
 GROUP BY j.id;
 
 --5. Donner le score moyen obtenu par chaque joueur, avec leur pseudo.
+SELECT j.pseudo, AVG(s.score) AS moyenne
+FROM joueurs AS j
+JOIN scores AS s ON j.id = s.id_joueur
+GROUP by j.id;
 
